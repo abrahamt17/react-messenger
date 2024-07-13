9c7687ee855093e4f61b480dedff76749b57ec87
abraham <abrahamtikuye@gmail.com>
2024-07-14 00:32:39 +0300

    debug

--- src/list/Chatlist/Chatlist.jsx
+++ src/list/Chatlist/Chatlist.jsx
@@ -3,66 +3,58 @@ import './Chatlist.css';
 import AddUser from './addUser/addUser';
 import { useUserStore } from '../../lib/userStore';
 import { onSnapshot } from 'firebase/firestore';
-// import Chats from '../../chat/Chats';
 import { doc } from 'firebase/firestore';
 import { db } from '../../lib/firebase';
 
+const Chatlist = () => {
+  const [adMode, setAddMode] = useState(false);
+  const [Chats, setChats] = useState([]);
 
-const Chatlist=()=> {
-const [adMode, setAddMode]= useState(false);
-const [Chats, setChats]= useState([]);
+  const { currentUser } = useUserStore();
 
-const{currentUser}= useUserStore();
+  useEffect(() => {
+    const unSub = onSnapshot(doc(db, "userchats", currentUser.id), (doc) => {
+      if (doc.exists()) {
+        setChats(Object.values(doc.data()));
+      } else {
+        setChats([]);
+      }
+    });
+    return () => {
+      unSub();
+    };
+  }, [currentUser.id]);
 
-useEffect(()=>{
-  const unSub=onSnapshot(doc(db, "userchats", currentUser.id), (doc)=>{
-    setChats(doc.data());
-  })
-  return ()=>{
-    unSub();
-  }
-}, [currentUser.id]);
-console.log(Chats);
-
+  console.log(Chats);
 
   return (
     <div>
-
-<div className='Chatlist'>
-<div className='search'>
-<div className="searchBar">
-<img src="./search.png"  alt="" />
-<input type='text' className=''  placeholder='search'/></div>
-<img
- className='add' 
-src={adMode?  './minus.png' : "./plus.png"} 
-alt='' 
-onClick={() =>setAddMode((prev)=> !prev)}/>
-
-
-</div>
-{Chats.map((chat)=>{
- 
- <div className='item' key={chat.chatId}>
-    <img src="./avatar.png" alt="" />
-     <div className='texts'>
-    <span>{chat.username }</span>
-    <h1>Hellooo</h1>
-     </div>
-  </div>
-
-})}
-
- 
-  
-  
-</div>
-{adMode && <AddUser />}
-  
-
+      <div className='Chatlist'>
+        <div className='search'>
+          <div className="searchBar">
+            <img src="./search.png" alt="" />
+            <input type='text' className='' placeholder='search' />
+          </div>
+          <img
+            className='add'
+            src={adMode ? './minus.png' : './plus.png'}
+            alt=''
+            onClick={() => setAddMode((prev) => !prev)}
+          />
+        </div>
+        {Chats.map((chat) => (
+          <div className='item' key={chat.chatId}>
+            <img src="./avatar.png" alt="" />
+            <div className='texts'>
+              <span>{chat.username}</span>
+              <h1>Hellooo</h1>
+            </div>
+          </div>
+        ))}
+      </div>
+      {adMode && <AddUser />}
     </div>
+  );
+};
 
-  )
-}
-
-export default Chatlist
+export default Chatlist;
